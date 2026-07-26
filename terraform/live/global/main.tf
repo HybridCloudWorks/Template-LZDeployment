c6b6c2b22f012d7c28bf3f2b4032e8e111c5e726
saulpatinojr <saulpatinojr@hotmail.com>
2026-07-25 23:09:57 -0500

    Implement non-prod workload layer and format Terraform (terraform/live/global/main.tf)

--- terraform/live/global/main.tf
+++ terraform/live/global/main.tf
@@ -9,7 +9,7 @@ terraform {
       version = "~> 4.2"
     }
   }
-  
+
   backend "azurerm" {
     # Configuration provided via backend.hcl
   }
@@ -23,26 +23,26 @@ provider "azurerm" {
 # Management Groups
 module "management_groups" {
   source = "../../modules/management-groups"
-  
-  org_prefix                      = var.org_prefix
-  identity_subscription_id        = var.identity_subscription_id
-  connectivity_subscription_id    = var.connectivity_subscription_id
-  management_subscription_id      = var.management_subscription_id
-  workload_prod_subscription_id   = var.workload_prod_subscription_id
+
+  org_prefix                       = var.org_prefix
+  identity_subscription_id         = var.identity_subscription_id
+  connectivity_subscription_id     = var.connectivity_subscription_id
+  management_subscription_id       = var.management_subscription_id
+  workload_prod_subscription_id    = var.workload_prod_subscription_id
   workload_nonprod_subscription_id = var.workload_nonprod_subscription_id
-  sandbox_subscription_id         = var.sandbox_subscription_id
+  sandbox_subscription_id          = var.sandbox_subscription_id
 }
 
 # Policy Baseline
 module "policy_baseline" {
   source = "../../modules/policy-baseline"
-  
-  root_mg_id         = module.management_groups.root_mg_id
+
+  root_mg_id               = module.management_groups.root_mg_id
   root_management_group_id = module.management_groups.root_mg_id
-  platform_mg_id     = module.management_groups.platform_mg_id
-  landingzones_mg_id = module.management_groups.landingzones_mg_id
-  sandbox_mg_id      = module.management_groups.sandbox_mg_id
-  allowed_locations  = var.allowed_locations
-  
+  platform_mg_id           = module.management_groups.platform_mg_id
+  landingzones_mg_id       = module.management_groups.landingzones_mg_id
+  sandbox_mg_id            = module.management_groups.sandbox_mg_id
+  allowed_locations        = var.allowed_locations
+
   depends_on = [module.management_groups]
 }
