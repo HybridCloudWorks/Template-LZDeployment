edb3982443eacc10f1823b4c743b8bc7f730129d
saulpatinojr <saulpatinojr@hotmail.com>
2026-07-29 00:41:56 -0500

    fix(ci): restore Factory CI contracts and Terraform formatting

--- factory/templates/terraform/modules/policy-baseline/outputs.tf
+++ factory/templates/terraform/modules/policy-baseline/outputs.tf
@@ -1,8 +1,8 @@
 output "policy_assignments" {
   description = "Map of policy assignment IDs"
   value = {
-    require_tags          = azurerm_management_group_policy_assignment.require_tags_root.id
-    allowed_locations     = azurerm_management_group_policy_assignment.allowed_locations.id
+    require_tags         = azurerm_management_group_policy_assignment.require_tags_root.id
+    allowed_locations    = azurerm_management_group_policy_assignment.allowed_locations.id
     nsg_on_subnets       = azurerm_management_group_policy_assignment.nsg_on_subnets.id
     sandbox_env_tag      = azurerm_management_group_policy_assignment.sandbox_tag.id
     sandbox_expiry       = azurerm_management_group_policy_assignment.sandbox_expiry.id
